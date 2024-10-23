635a900bc321ea795556ce845f49fa4242223f8e
253503 <gayathri.mohandoss@ust.com>
2024-10-23 16:26:50 +0530

    Day02

---
+++ UST_Training/Day_02/FunwithFunctions.py
@@ -0,0 +1,8 @@
+#def say_hello(name,n):
+   # return name[n-1]
+
+def say_hello(name):
+     return name[::-1]
+
+
+print(say_hello("Gayathri"))
---
+++ UST_Training/HelloNumeric.py
@@ -0,0 +1,15 @@
+print(2+2)  
+
+print(3-1)
+
+print(3*3)
+
+print(15/3)
+
+print(3**3)
+
+print(15%2)
+
+print(10//2)
+
+print("results shown")
