16b98882504ad367a1dffb4598df49991848c8e3
253503 <gayathri.mohandoss@ust.com>
2024-11-13 14:10:21 +0530

    DAy12 , 11 and titanic added

--- UST_Training/Day_12/pivotTb.py
+++ UST_Training/Day_12/pivotTb.py
@@ -10,21 +10,29 @@ data={
 df=pd.DataFrame(data)
 
 #totalsales per store
-df1=df.groupby('Store').sum()
-print(df1)
+#df1=df.groupby('Store').sum()
+#print(df1)
 
+
+#Method 1
 #totalsales per Region
-df2=df.groupby('Region').sum().reset_index()
-print(df2)
+# df2=df.groupby('Region').sum().reset_index()
+# print(df2)
 
+# #merge orginal and region dt
+# df3=pd.merge(df,df2,on='Region',how='left',suffixes=("_store","_region"))
+# print(df3)
 
-#merge orginal and region dt
-df3=pd.merge(df,df2,on='Region',how='left',suffixes=("_store","_region"))
-print(df3)
+#Method2
+regional_sales=df.groupby(['Region']).agg({'Sales':'sum'}).reset_index()
 
+sales_aggregated_df=df.groupby(['Store','Region']).agg({'Sales':'sum'}).reset_index()
+#print(sales_aggregated_df)
 
-#sales percentage of sales and region
-#df=df['Store']/df['Region'] *100
+merged_Df=pd.merge(sales_aggregated_df,regional_sales,on='Region',how='left',suffixes=("_store","_region"))
+merged_Df['SalesbyRegion%']=merged_Df['Sales_store']/merged_Df['Sales_region'] * 100
+#print(merged_Df)
 
-df3['new column']=df2['Sales']/df3['Sales_region'] 
-print(df3)
+df=pd.merge(df,merged_Df,left_on=['Store','Region'],right_on=['Store','Region'],how='left')
+
+print(df)
