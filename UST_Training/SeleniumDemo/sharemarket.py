63ea481c1387b9bc98835e4dc4b552e52ba5838d
253503 <gayathri.mohandoss@ust.com>
2024-12-02 11:26:04 +0530

    selenium Demo 2/12

---
+++ UST_Training/SeleniumDemo/sharemarket.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver_path = r'geckodriver.exe'
+
+service = Service(driver_path)
+
+driver = webdriver.Firefox(service = service)
+driver.maximize_window()
+driver.get("https://www.moneycontrol.com/india/stockpricequote/refineries/relianceindustries/RI")
+driver.implicitly_wait(5)
+
+driver.refresh()
+time.sleep(5)
+driver.refresh()
+
+navElement = driver.find_element(By.ID,"advchart")
+driver.implicitly_wait(5)
+time.sleep(5)
+navElement.screenshot('shareMarket.jpg')
+time.sleep(2)
